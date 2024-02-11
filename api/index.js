48730f474b7ce01233f599f8385f26de6d173cc5
shubham <shubham992284@gmail.com>
2024-02-11 19:00:30 +0530

    express server started on port no 8000

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
-console.log("hello shubham");
+const express=require('express');
+const port=8000;
+
+const app=express();
+
+app.listen(port,(err)=>{
+    console.log("server start on port no: "+port);
+})
+
